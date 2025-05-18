1514abda66678e6bad71b17a59736e78b9024e49
Abhirup Roy Karmakar <abhiruprk@gmail.com>
2025-05-18 21:26:17 +0200

    Update installs.sh

--- run_once_before_installs.sh
+++ run_once_before_installs.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
 echo -e "\n*** INSTALLING MISSING PROGRAMS ***"
 
-### Install starship
-if ! command -v starship >/dev/null 2>&1; then
-    echo "starship is not installed. Running installer script..."
-    curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir $HOME/.local/bin
+### Install Oh-My-ZSH
+if ! command -v omz >/dev/null 2>&1; then
+    echo "Oh-My-ZSH is not installed. Running installer script..."
+    sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 else
-    echo "starship is already installed. Skipping installation."
+    echo "Oh-My-ZSH is already installed. Skipping installation."
 fi
 
-### Install pyenv
+### Install Starship
+if ! command -v starship >/dev/null 2>&1; then
+    echo "Starship is not installed. Running installer script..."
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir $HOME/.local/bin
+else
+    echo "Starship is already installed. Skipping installation."
+fi
+
+### Install Pyenv
 if ! command -v pyenv >/dev/null 2>&1; then
-    echo "pyenv is not installed. Running installer script..."
+    echo "Pyenv is not installed. Running installer script..."
     curl -fsSL https://pyenv.run | bash
     git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
 else
-    echo "pyenv is already installed. Skipping installation."
+    echo "Pyenv is already installed. Skipping installation."
 fi
 
-### Install micromamba
+### Install Micromamba
 if ! command -v micromamba >/dev/null 2>&1; then
-    echo "micromamba is not installed. Running installer script..."
+    echo "Micromamba is not installed. Running installer script..."
     curl -Ls https://micro.mamba.pm/install.sh | bash -s -- -b -p "$HOME/.local"
 else
-    echo "micromamba is already installed. Skipping installation."
+    echo "Micromamba is already installed. Skipping installation."
 fi
 
 echo -e "\n*** PLEASE RESTART THIS SHELL TO APPLY NEW INSTALLATIONS ***"
