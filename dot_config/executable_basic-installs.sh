1aa9f7437a6f4df2d6c0ccbf73c0632705874b20
Abhirup Roy Karmakar <abhiruprk@gmail.com>
2025-05-18 19:32:15 +0200

    Update .config/basic-installs.sh

--- dot_config/executable_basic-installs.sh
+++ dot_config/executable_basic-installs.sh
@@ -5,7 +5,7 @@ if ! command -v starship >/dev/null 2>&1; then
     echo "starship is not installed. Running installer script..."
     curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir $HOME/.local/bin
 else
-    echo "starship is installed. Skipping installation."
+    echo "starship is already installed. Skipping installation."
 fi
 
 ### Install pyenv
@@ -14,9 +14,15 @@ if ! command -v pyenv >/dev/null 2>&1; then
     curl -fsSL https://pyenv.run | bash
     git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
 else
-    echo "pyenv is installed. Skipping installation."
+    echo "pyenv is already installed. Skipping installation."
 fi
 
+### Install micromamba
+if ! command -v micromamba >/dev/null 2>&1; then
+    echo "micromamba is not installed. Running installer script..."
+    curl -Ls https://micro.mamba.pm/install.sh | bash -s -- -b -p "$HOME/.local"
+else
+    echo "micromamba is already installed. Skipping installation."
+fi
 
-echo -e "\nPLEASE RESTART THIS SHELL TO APPLY NEW INSTALLATIONS."
 
