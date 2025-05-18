0c95626244b3fdd6ced6f8a82c711a78ce897fb8
Abhirup Roy Karmakar <abhiruprk@gmail.com>
2025-05-18 19:06:06 +0200

    Update .config/basic-installs.sh

--- dot_config/executable_basic-installs.sh
+++ dot_config/executable_basic-installs.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+### Install starship
+if ! command -v starship >/dev/null 2>&1; then
+    echo "starship is not installed. Running installer script..."
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir $HOME/.local/bin
+else
+    echo "starship is installed. Skipping installation."
+fi
+
 ### Install pyenv
 if ! command -v pyenv >/dev/null 2>&1; then
     echo "pyenv is not installed. Running installer script..."
